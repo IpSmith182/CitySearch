6d0c30fc1e27a51f305cd6ae5a947f6220d767cc
IpSmith182 <74120352+IpSmith182@users.noreply.github.com>
2020-11-08 12:14:16 +0000

    Add files via upload

---
+++ ICityResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CitySearch.Models
+{
+    interface ICityResult
+    {
+        ICollection<string> NextLetters { get; set; }
+        ICollection<string> NextCities { get; set; }
+    }
+}
